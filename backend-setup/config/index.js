4bdc68b6ea963de890a4a47d3d437dcd58791149
JC <J0shua.chen@yahoo.com>
2017-08-14 01:37:24 -0700

    testing

--- backend-setup/config/index.js
+++ backend-setup/config/index.js
@@ -2,7 +2,7 @@ var configValues = require('./config'); // ./ means it's local
 
 module.exports = {
   getDBConnectionString: function() {
-    return 'mongodb://' + configValues.username + ':' + configValues.password + '@ds057234.mlab.com:57234/nodetodosample';
+    return 'mongodb://herokuhost/' + configValues.username + ':' + configValues.password + '@ds057234.mlab.com:57234/nodetodosample';
   }
 };
 
